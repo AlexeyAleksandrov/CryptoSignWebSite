879d0c6931ba0ae8029d80fe13054f0b59b10315
AlexeyAleksandrov <chinok73@yandex.ru>
2022-10-04 20:21:38 +0300

    Добавлены стили на главную страницу и страницу авторизации

--- src/main/java/com/webapi/application/security/SecurityConfiguration.java
+++ src/main/java/com/webapi/application/security/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,7 +19,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter
 {
-    String[] allowMatchers = {"/", "/login", "/auth", "/signup", "/index", "/upload", "/download**"};
+    String[] allowMatchers = {"/", "/login", "/auth", "/signup", "/index", "/upload", "/download**", "/css**"};
 
     // сервис для конвертации пользователей в UserDetails
     final SecurityUserDetailsService securityUserDetailsService;
@@ -34,7 +35,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter
         http
                 .csrf().disable()   // отключаем csrf
                 .authorizeRequests()
-//                .antMatchers("/").permitAll()
+//                .antMatchers("/**").permitAll()
                 .antMatchers(allowMatchers).permitAll()
                 .anyRequest()
                 .authenticated()
@@ -69,5 +70,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter
     protected void configure(AuthenticationManagerBuilder auth) throws Exception
     {
         auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception
+    {
+        web.ignoring().antMatchers("/css/**","/js/**","/fonts/**","/images/**");
     }
 }
